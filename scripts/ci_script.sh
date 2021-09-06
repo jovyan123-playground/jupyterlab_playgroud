4f713a0a098d05867c15db627520dd47a3eef1d7
snuffy <41898282+github-actions[bot]@users.noreply.github.com>
2021-09-06 04:31:29 -0500

    cache eslint

--- scripts/ci_script.sh
+++ scripts/ci_script.sh
@@ -64,8 +64,9 @@ fi
 
 if [[ $GROUP == lint ]]; then
     # Lint our files.
-    jlpm run lint:check || (echo 'Please run `jlpm run lint` locally and push changes' && exit 1)
-    jlpm run eslint:check:typed || (echo echo 'Please run `jlpm run eslint:typed` locally and push changes' && exit 1)
+    jlpm run prettier:check || (echo 'Please run `jlpm run prettier` locally and push changes' && exit 1)
+    jlpm run eslint:check --cache-location ~/.cache/eslint || (echo 'Please run `jlpm run eslint` locally and push changes' && exit 1)
+    jlpm run eslint:check:typed --cache-location ~/.cache/eslint || (echo echo 'Please run `jlpm run eslint:typed` locally and push changes' && exit 1)
 fi
 
 
