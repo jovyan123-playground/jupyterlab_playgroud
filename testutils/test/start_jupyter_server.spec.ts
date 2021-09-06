f6ccb0fa1b72b4abba78da1d9aa0f79b425ff223
snuffy <41898282+github-actions[bot]@users.noreply.github.com>
2021-09-06 04:04:35 -0500

    wip

--- testutils/test/start_jupyter_server.spec.ts
+++ testutils/test/start_jupyter_server.spec.ts
@@ -18,7 +18,9 @@ describe('JupyterServer', () => {
   it('should accept options', async () => {
     jest.setTimeout(20000);
     const pageConfig = { foo: 'bar', fizz: 'buzz' };
-    const configData = { 'FakeTrait': { 'fake_prop': 1 }, 'OtherTrait': { 'other_prop': 'hello' }};
+    const configData = { FakeTrait: { fake_prop: 1 }, OtherTrait: { other_prop: 'hello' }, KernelManager: {
+          shutdown_wait_time: 1.11
+    }};
     const additionalKernelSpecs = { 'foo': {
       argv: ['python', '-m', 'ipykernel_launcher', '-f', '{connection_file}'],
       display_name: 'Test Python',
@@ -31,6 +33,7 @@ describe('JupyterServer', () => {
     expect(PageConfig.getOption('fizz')).toEqual('buzz');
     expect(PageConfig.getOption('__configData')).toContain('FakeTrait');
     expect(PageConfig.getOption('__configData')).toContain('OtherTrait');
+    expect(PageConfig.getOption('__configData')).toContain('1.11');
     expect(PageConfig.getOption('__kernelSpec_foo')).toContain('Test Python');
     await expect(server.shutdown()).resolves.not.toThrow();
   });
